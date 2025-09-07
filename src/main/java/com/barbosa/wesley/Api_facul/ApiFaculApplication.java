179ee55d22545e4b5006bb3b6413a0cbb9b923bf
WMBarbosa <wesleymario01@gmail.com>
2025-09-07 11:44:18 -0300

    initial commit

---
+++ src/main/java/com/barbosa/wesley/Api_facul/ApiFaculApplication.java
@@ -0,0 +1,13 @@
+package com.barbosa.wesley.Api_facul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFaculApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFaculApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbosa/wesley/Api_facul/ApiFaculApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosa.wesley.Api_facul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFaculApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
